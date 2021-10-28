65e3cea5ea7d135cc7cec23e07765ed9990cef0d
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 09:19:50 +0530

    Added exception for no name found

--- urldl.py
+++ urldl.py
@@ -1,11 +1,13 @@
 import requests
 
 
-def download(url, name):
+def download(url, name=""):
     try:
         response = requests.get(url)
+        if not name:
+            return raise Exception("No name found")
         with open(name, "wb") as file:
             file.write(response.content)
         return name 
     except Exception as error:
-        raise Exception(error)
+        return raise Exception(error)
